6bd94d7839c0f60440c5da4f74c20c1682e6786d
brokenPattern <michal.neumann@yahoo.pl>
2019-10-29 23:11:18 +0100

    Updates

---
+++ precache-manifest.c31eb3c18be2f21f582234f9f67957f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fc8b68814ee94428f59cbdd34bb56f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07f5d8e0f66946e680c7",
+    "url": "/static/css/main.61f881d6.chunk.css"
+  },
+  {
+    "revision": "ed2e794c9e46c70a011f",
+    "url": "/static/js/2.70294702.chunk.js"
+  },
+  {
+    "revision": "07f5d8e0f66946e680c7",
+    "url": "/static/js/main.08c15eda.chunk.js"
+  },
+  {
+    "revision": "ed4750f41f4543566353",
+    "url": "/static/js/runtime-main.fbe98da3.js"
+  }
+]);
